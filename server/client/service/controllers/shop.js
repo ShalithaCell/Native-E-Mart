396e165cf0c7295068a80de68cc9f41001f8db33
shalitha <shalithax@gmail.com>
2021-05-14 22:06:46 +0530

    change project structure

---
+++ server/client/service/controllers/shop.js
@@ -0,0 +1,5 @@
+
+$( document ).ready(function() {
+    const header = renderNavigationBar('shop');
+    $("#header").append( header );
+});
--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const koaBody = require('koa-body');
 const KoaStatic = require('koa-static');
+const serve = require('koa-static');
 const router = require('./api');
 const { dbContext, exceptionService, applicationDataSeeder } = require('./services');
 
@@ -15,6 +16,7 @@ app
     .use(exceptionService.jsonErrorHandler) // register json error handler middleware
     .use(router()) // Use the Router on the sub routes
     .use(KoaStatic('public')) // server statics
+    .use(serve('client'))
     // Bootstrap the server
     .listen(process.env.PORT || 5000, () =>
     {
@@ -25,4 +27,11 @@ app
             .then((r) => console.info('role data seeder executed'));
         applicationDataSeeder.seedUsers()
             .then((r) => console.info('user data seeder executed'));
+
+        console.log();
+        console.log();
+        console.log('=======================Application StartUp===========================');
+        console.log('\x1b[33m\x1b[4m%s\x1b[0m', 'http://localhost:5000/');
+        console.log('=====================================================================');
+        console.log();
     });
